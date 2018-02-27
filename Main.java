03c39acd95e4e4a71b11e2194996ecffc68c300f
apokkalyps <32598875+apokkalyps@users.noreply.github.com>
2018-02-27 18:11:01 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,8 @@
-public class Main{}
+public class Main{
+  public static void main (String[] args){
+    Signal signal1 = new Signal();
+    Signal signal2 = new Signal();
+    
+    Generateur gener = new Generateur(signal1, signal2);
+  }
+}
