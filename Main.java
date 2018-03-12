4327b87445eb745be4406f90a8d5ba6941b39e06
mr-fish77 <mathieu.rocfort@orange.fr>
2018-03-12 11:15:51 +0100

    affichage signal rectangle

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ public class Main{
 	public static void main (String[] args){
 		/* On cree les deux signaux. */
 		Signal[] signaux = new Signal[2];
-		signaux[0] = new Sinus(1);
-		signaux[1]= new Sinus(2);
+		signaux[0] = new Rectangle(1);
+		signaux[1]= new Rectangle(2);
 		
 		new Generateur (signaux);
 		new Oscilloscope (signaux);
--- Sinus.java
+++ Sinus.java
@@ -4,7 +4,16 @@
  *
  */
 public class Sinus extends Signal {
+    private final int NB_POINTS = 50; //np de point entre chaque graduation
+    private final int CASE_X = 10; //nb de graduation selon X
+    private final int CASE_Y = 10; //nb de graduation selon Y
+    public double echelleX; //valeur d'une graduation selon X en s
+    public double echelleY; //valeur d'une graduation selon Y en V
     
+    public  int nbPixelX = 75; // nb de pixel par graduation X
+    public  int nbPixelY = 75; // nb de pixel par graduation Y
+    public  int ox = 300; // origine
+    public  int oy =300; // origine
    
 
     public static void main (String[] args){
@@ -15,7 +24,7 @@ public class Sinus extends Signal {
     
     public Sinus(int n){
         super(n);
-        echelleX = 1;
+        echelleX = 5;
         echelleY = 5;
         this.nuagePoint = new double[NB_POINTS * CASE_X ][2]; 
     }
@@ -33,8 +42,8 @@ public class Sinus extends Signal {
                 nuagePoint[i + ((NB_POINTS * CASE_X)/2)][0] = (double) ( (  (((i * echelleX))/ NB_POINTS)) * nbPixelX / this.echelleX )   + ox; //mise à l'echlle des x
                 nuagePoint[i + ((NB_POINTS * CASE_X)/2)][1] = (double ) (this.getAmplitude() * Math.sin( getFreq() * 2.* Math.PI * ((i * echelleX)/ NB_POINTS))   * nbPixelY / this.echelleY ) + oy;  //calcul de y en prenant compte de l'échelle 
                 
-                //System.out.print( i +  "  x = " + nuagePoint[i + ((NB_POINTS * CASE_X)/2)][0] ); //return pour debug
-                //System.out.println ( "   y = " + nuagePoint[i + ((NB_POINTS * CASE_X)/2)][1] );
+                System.out.print( i +  "  x = " + nuagePoint[i + ((NB_POINTS * CASE_X)/2)][0] ); //return pour debug
+                System.out.println ( "   y = " + nuagePoint[i + ((NB_POINTS * CASE_X)/2)][1] );
             
         }
        
