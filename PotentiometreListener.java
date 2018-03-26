27beae21c9d7dc3323f93a729faec5c41464d0cc
apokkalyps <32598875+apokkalyps@users.noreply.github.com>
2018-03-26 09:38:35 +0200

    Update PotentiometreListener.java

--- PotentiometreListener.java
+++ PotentiometreListener.java
@@ -1,3 +1,8 @@
+/** Interface d'ecoute liee a un Potentiometre. */
 public interface PotentiometreListener {
+	
+	/** Action declenchee lorsque la valeur du Potentiometre est modifiee.
+	 * @param potentiometre La source de l'evenement.
+	 * @param evolutionEcran Nouvelle valeur du potentiometre. */
 	public void potentiometrePerformed(Potentiometre potentiometre, int evolutionCran);
 }
