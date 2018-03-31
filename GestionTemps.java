1a240de55a2275c150462fb8a05f852a0122249f
apokkalyps <32598875+apokkalyps@users.noreply.github.com>
2018-03-31 20:30:50 +0200

    Commentaires classe GestionTemps

--- GestionTemps.java
+++ GestionTemps.java
@@ -12,22 +12,26 @@ import javax.swing.JPanel;
  */
 public class GestionTemps extends JPanel implements PotentiometreListener{
 	/** Le tableau de signal */
-	private Signal[] signaux;		//tableau de signal (on est oblige car on cree souvent de nouveaux signaux)
+	private Signal[] signaux;
 	
-	/** Les boutons d'interactions */
-	private Potentiometre potPos;	//potentiometre position verticale
-	private Potentiometre potDiv;	//potentiometre volts/div
-	private BoutonTexte chMenu;		//bouton affichage menu du signal
+	/** Objet interactif : potentiometre de position verticale. */
+	private Potentiometre potPos;
+	/** Objet interactif : potentiometre de volts/division. */
+	private Potentiometre potDiv;
+	/** Objet interactif : bouton d'affichage du menu du signal. */
+	private BoutonTexte chMenu;
 	
 	/** L'ecran pour gerer les repaints */
-	private Ecran ecran;			//Ecran de l'oscillo pour gerer les repaint() et le bas de l'ecran
+	private Ecran ecran;
 	
-	/** Les echelles et decalages */
-	private double decalage;		//decalage des signaux en temps
-	//echelles en chiffres et lettres
+	/** Decallage des signaux en temps. */
+	private double decalage;
+	/** Echelle en chiffres. */
 	private static final double[] ECHELLES = {1, 0.5, 0.2, 0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001, 0.00005, 0.00001, 0.000005, 0.000001, 0.0000005, 0.0000001};
+	/** Echelle en toutes lettres. */
 	private static final String[] STR_ECHELLES = {"1 s", "0.5 s", "0.2 s", "0.1 s", "50 ms", "10 ms", "5 ms", "1 ms", "0.5 ms", "0.1 ms", "50 micro s", "10 micro s", "5 micro s", "1 micro s", "0.5 micro s", "0.1 micro s"};
-	private int compteurEchelle = 0;	//pour connaitre la position dans le tableau
+	/** Index actuel dans le tableau des echelles. */
+	private int compteurEchelle = 0;
 	
 	
 	/**
@@ -103,7 +107,7 @@ public class GestionTemps extends JPanel implements PotentiometreListener{
 	public void autoset() {
 		int compteur = 0;
 		
-		//on diminue l'echelle jusqu'a ce que 2 periodes
+		//on diminue l'echelle jusqu'a voir 4 periodes
 		while(
 				compteur < ECHELLES.length && 
 				ECHELLES[compteur]*Signal.CASE_X*signaux[0].getFreq() > 4) 
@@ -112,11 +116,12 @@ public class GestionTemps extends JPanel implements PotentiometreListener{
 		}
 		
 		compteurEchelle = compteur;
-		mAJEchelle();	//on met a jour les donnees puis affichage
+		mAJEchelle();
 	}
 	
 	
-	/** Methode unifiee de mise a l'echelle
+	/** Methode unifiee de mise a l'echelle.
+	 * Concretement, mise a jour des donnees puis affichage.
 	 */
 	public void mAJEchelle() {
 		signaux[0].echelleX = ECHELLES[compteurEchelle];
@@ -131,16 +136,17 @@ public class GestionTemps extends JPanel implements PotentiometreListener{
 	 * @param int evolutionCran : + ou -1
 	 */
 	public void potentiometrePerformed(Potentiometre potentiometre, int evolutionCran) {
-		if(potentiometre.equals(potPos)) {	//si c'est le potentiometre de position
-			decalage += 0.04*evolutionCran;	//on implemente/reduit le decalage (0.04 correspond au reglage sur le vrai oscillo)
-			//on l'attribue a chacun des signaux
+		if(potentiometre.equals(potPos)) {
+			decalage += .04*evolutionCran;	//on implemente/reduit le decalage (0.04 correspond au reglage sur le vrai oscillo)
 			signaux[0].decalageX = decalage;
 			signaux[1].decalageX = decalage;
 			ecran.bas.setText("Decalage temps : " + String.format("%.2f", (double)(Math.round(decalage*100))/100) + " div");
 			ecran.grille.repaint();
 			
-		}else if(potentiometre.equals(potDiv)){	//reglage echele
-			if((compteurEchelle >= 0 && compteurEchelle < (ECHELLES.length - 1) && evolutionCran > 0) || (compteurEchelle < (ECHELLES.length) && compteurEchelle > 0 && evolutionCran < 0)) {	//on regarde qu'on est toujours dans les clous du tableau
+		} else if(potentiometre.equals(potDiv)){
+			if ( //on regarde qu'on est toujours dans les clous du tableau
+					(compteurEchelle >= 0 && compteurEchelle < (ECHELLES.length - 1) && evolutionCran > 0) || 
+					(compteurEchelle < ECHELLES.length && compteurEchelle > 0 && evolutionCran < 0)) {
 				compteurEchelle += evolutionCran;
 				//attribution au 2 signaux
 				mAJEchelle();
